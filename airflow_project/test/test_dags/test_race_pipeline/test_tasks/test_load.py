b4c43facb52b9c91dd3b2545444fed3b19c5d2de
twardy7991 <patryktwardowski79@gmail.com>
2026-01-19 11:47:44 +0100

    added custom postgres hook and improved test_load.py

--- airflow_project/test/test_dags/test_race_pipeline/test_tasks/test_load.py
+++ airflow_project/test/test_dags/test_race_pipeline/test_tasks/test_load.py
@@ -1,25 +1,59 @@
 from airflow_project.dags.race_pipeline.tasks.load import load
-from airflow_project.plugins.hooks.my_postgres_hook import MyPostgresHook
+
 import pandas as pd
 from pandas.testing import assert_frame_equal
 import pytest
 from pytest_mock.plugin import MockerFixture
-from airflow.sdk import Connection
+from airflow.models import Connection
+from airflow.utils import db
+
+import sqlalchemy 
+from sqlalchemy.engine import Engine
+from contextlib import contextmanager
 
 import os
 
-def create_connection() -> None:
-    conn_uri = "postgresql+psycopg2://postgres:postgres@localhost:5452/test_f1_db"
-    return Connection(uri=conn_uri)
+conn_uri = "postgresql+psycopg2://postgres:postgres@localhost:5452/test_f1_db"
+
+def create_connection() -> Connection:
+    return Connection(
+            conn_id="test_database", 
+            conn_type="postgres",
+            host="postgresql+psycopg2",
+            schema="test_f1_db",
+            login="airflow",
+            password="airflow",
+            port=5432
+            )
+
+@contextmanager
+def get_conn():
+    engine = sqlalchemy.create_engine(conn_uri)
+    conn = engine.connect()
+
+    try:
+        yield conn
+    finally:
+        conn.close()
     
 def test_load(mocker : MockerFixture, monkeypatch):
     
     ti = mocker.MagicMock()
     
-    def mock_get_conn(*args, **kwargs):
-        return create_connection(MyPostgresHook, "get_conn", mock_get_conn)
+    # Mock the get_connection to avoid database lookup
+    test_conn = Connection(
+        conn_id="POSTGRES_TEST_CONN",
+        conn_type="postgresql",
+        host="localhost",
+        schema="test_f1_db",
+        login="postgres",
+        password="postgres",
+        port=5452
+    )
     
-    monkeypatch.setattr()
+    print(test_conn.get_uri())
+    
+    mocker.patch('airflow.sdk.bases.hook.BaseHook.get_connection', return_value=test_conn)
     
     def xcom_pull_side_effect(*, task_ids=None, key=None, **_):
         xcom_map = {
@@ -27,23 +61,60 @@ def test_load(mocker : MockerFixture, monkeypatch):
         }
         return xcom_map[(task_ids, key)]
     
-    # patching the xcom_pull method with money function
+    # patching the xcom_pull method with monkey function
     ti.xcom_pull.side_effect = xcom_pull_side_effect
     
     context = {"ti" : ti,
         "year" : 2023,
         "gp_name" :"Bahrain Grand Prix",
+        "conn_id" : "POSTGRES_TEST_CONN",
+        "load_type" : "replace"
     }
 
     result = load.function(
         **context
     )
     
-    print(result)
+    with get_conn() as conn:
+        data_created = pd.read_sql(
+            "select * from session_data",
+            dtype_backend="pyarrow",
+            con=conn
+        )
     
-
-        
-        
+    data_expected = pd.read_parquet("airflow_project/test/data/processed/Bahrain Grand Prix/session_dataset.parquet",
+                                engine="pyarrow", 
+                                dtype_backend="pyarrow"
+                            )
+    
+    data_expected["Year"] = 2023
+    data_expected["Race"] = "Bahrain Grand Prix"
+    data_expected.columns = [col.lower() for col in data_expected.columns]
+    
+    data_expected = data_expected.reindex(sorted(data_expected.columns), axis=1)
+    
+    data_expected.reset_index(drop=True, inplace=True)
+    data_created.reset_index(drop=True, inplace=True)
+    
+    print("data_expected",
+        data_expected[
+            (data_expected["driver"] == "LEC") &
+            (data_expected["lapnumber"] == 40)
+        ]["laptime"]
+    )
+    
+    print("data_created",
+        data_created[
+            (data_created["driver"] == "LEC") &
+            (data_created["lapnumber"] == 40)
+        ]["laptime"]
+    )
+    
+    print("\nexpected" , data_expected[data_expected["laptime"].isna()])
+    print("\ncreated", data_created[data_created["laptime"].isna()])
+    
+    assert_frame_equal(data_created, data_expected, check_dtype=False)
+    
 # def test_get_session(race_extractor : RaceExtractor):
     
 #     year = 2023
