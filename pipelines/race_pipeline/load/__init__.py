e91e9a82c5bfc563c914fcd4c980a191b43dcaa6
twardy7991 <patryktwardowski79@gmail.com>
2026-01-19 13:56:32 +0100

    deleted legacy code

--- pipelines/race_pipeline/load/__init__.py
+++
@@ -1,5 +0,0 @@
-from pipelines.race_pipeline.load.race_loader import RaceLoader
-
-__all__ = [
-    "RaceLoader"
-]
--- pipelines/season_pipeline/load/season_loader.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-import pandas as pd
-
-class SeasonLoader:
-    def __init__(self):
-        pass
-    
-    def save_season_dataset(self, df : pd.DataFrame) -> None:        
-        df.to_pickle('season_full_NAs.pkl')
